ddb4941393d29d2505f862013c749ebd0f01f1e5
Gordon Inggs <gordon.inggs@gmail.com>
2020-08-05 13:57:42 +0200

    Initial implementation of geocoding functionality

--- src/server/cape_of_good_place_names/__init__.py
+++ src/server/cape_of_good_place_names/__init__.py
@@ -1,2 +1,5 @@
+from geocode_array import Nominatim, CCT, ArcGIS
+
 TIMEZONE = "Africa/Johannesburg"
 REQUEST_ID_UNIQUE_VALUE_PREFIX = "cogpn-"
+GEOCODERS = [Nominatim.Nominatim, CCT.CCT, ArcGIS.ArcGIS,]
--- src/server/cape_of_good_place_names/controllers/geocode_controller.py
+++ src/server/cape_of_good_place_names/controllers/geocode_controller.py
@@ -1,10 +1,14 @@
+import collections
 import logging
 import pprint
 
 import connexion
+from flask import current_app, json
+from geocode_array import geocode_array
 import six
 
 from cape_of_good_place_names.models.error import Error  # noqa: E501
+from cape_of_good_place_names.models.geocode_result import GeocodeResult
 from cape_of_good_place_names.models.geocode_results import GeocodeResults  # noqa: E501
 from cape_of_good_place_names import util
 
@@ -19,13 +23,36 @@ def geocode(address):  # noqa: E501
 
     :rtype: GeocodeResults
     """
-    request_id = util.get_request_uuid()
     request_timestamp = util.get_timestamp()
 
+    # Actually doing the geocoding
+    geocoders = [gc() for gc in current_app.config["GEOCODERS"]]
+    geocoder_results = (
+        (
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [result[1], result[2]]
+                },
+                "properties": {
+                    "address": result[0]
+                }
+            } if result[1] is not None
+            else None
+        )
+        for result in geocode_array.threaded_geocode(geocoders, address)
+    )
+
+    response_results = [
+        GeocodeResult(geocoder.__class__.__name__, json.dumps(geocoder_result), 1 if geocoder_result else 0)
+        for geocoder, geocoder_result in zip(geocoders, geocoder_results)
+    ]
+
     response = GeocodeResults(
-        id=request_id,
+        id=util.get_request_uuid(),
         timestamp=request_timestamp,
-        results=[]
+        results=response_results
     )
 
     return response
