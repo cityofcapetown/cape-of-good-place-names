a69f7d3236b0617d2f72e1ad7585c5483fea1d9c
Gordon Inggs <gordon.inggs@gmail.com>
2020-08-05 09:21:54 +0200

    Adding auth header to tests to make them work.

--- src/server/cape_of_good_place_names/test/test_default_controller.py
+++ src/server/cape_of_good_place_names/test/test_default_controller.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 
 from __future__ import absolute_import
+import base64
 
 from flask import json
 from six import BytesIO
@@ -15,6 +16,10 @@ from cape_of_good_place_names.test import BaseTestCase
 class TestDefaultController(BaseTestCase):
     """DefaultController integration test stubs"""
 
+    def setUp(self) -> None:
+        credentials = base64.b64encode(b"test_user:test_password").decode('utf-8')
+        self.authorisation_headers = {"Authorization": f"Basic {credentials}"}
+
     def test_geocode(self):
         """Test case for geocode
 
@@ -24,7 +29,9 @@ class TestDefaultController(BaseTestCase):
         response = self.client.open(
             '/v1/geocode',
             method='GET',
-            query_string=query_string)
+            query_string=query_string,
+            headers=self.authorisation_headers
+        )
         self.assert200(response,
                        'Response body is : ' + response.data.decode('utf-8'))
 
@@ -38,7 +45,9 @@ class TestDefaultController(BaseTestCase):
         response = self.client.open(
             '/v1/boundary_lookup',
             method='GET',
-            query_string=query_string)
+            query_string=query_string,
+            headers=self.authorisation_headers
+        )
         self.assert200(response,
                        'Response body is : ' + response.data.decode('utf-8'))
 
@@ -51,11 +60,13 @@ class TestDefaultController(BaseTestCase):
         response = self.client.open(
             '/v1/scrub',
             method='GET',
-            query_string=query_string)
+            query_string=query_string,
+            headers=self.authorisation_headers)
         self.assert200(response,
                        'Response body is : ' + response.data.decode('utf-8'))
 
 
 if __name__ == '__main__':
     import unittest
+
     unittest.main()
