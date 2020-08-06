630b295d1c0f6601cda4e05b5fb481dc250c62b7
Gordon Inggs <gordon.inggs@gmail.com>
2020-08-06 18:48:07 +0200

    Adding GeoLookup functionality
    * Either looks up a specific feature in the specified dataset, or returns the whole dataset
    * Happy case tests for both!

--- src/server/cape_of_good_place_names/__init__.py
+++ src/server/cape_of_good_place_names/__init__.py
@@ -3,3 +3,8 @@ from geocode_array import Nominatim, CCT, ArcGIS
 TIMEZONE = "Africa/Johannesburg"
 REQUEST_ID_UNIQUE_VALUE_PREFIX = "cogpn-"
 GEOCODERS = [Nominatim.Nominatim, CCT.CCT, ArcGIS.ArcGIS,]
+
+GEOLOOKUP_DATASET_DIR = "/data/lookup_layers"
+GEOLOOKUP_DATASET_CONFIG = {
+    # Layer ID: (Layer filename, Property ID attribute)
+}
