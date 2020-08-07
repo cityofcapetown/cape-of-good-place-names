0f654d46c0765de6f2f9028ef671c0c16ea1824f
Gordon Inggs <gordon.inggs@gmail.com>
2020-08-07 08:16:39 +0200

    Adding basic scrubber to server implementation

--- src/server/cape_of_good_place_names/__init__.py
+++ src/server/cape_of_good_place_names/__init__.py
@@ -1,10 +1,17 @@
 from geocode_array import Nominatim, CCT, ArcGIS
+from basic_scrubber import BasicScrubber
 
 TIMEZONE = "Africa/Johannesburg"
 REQUEST_ID_UNIQUE_VALUE_PREFIX = "cogpn-"
+
+# Geocoder config
 GEOCODERS = [Nominatim.Nominatim, CCT.CCT, ArcGIS.ArcGIS,]
 
+# GeoLookup config
 GEOLOOKUP_DATASET_DIR = "/data/lookup_layers"
 GEOLOOKUP_DATASET_CONFIG = {
     # Layer ID: (Layer filename, Property ID attribute)
 }
+
+# Scrub config
+SCRUBBERS = [BasicScrubber.BasicScrubber]
